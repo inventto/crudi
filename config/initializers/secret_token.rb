08d0071d9722a3b04502de20ef00f1eb2cd9a795
Jônatas Paganini <jonatasdp@gmail.com>
2013-05-24 14:42:16 -0300

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Crudi::Application.config.secret_key_base = '1efb3aadbd601c502a55cac924fc95f81f2a7449c04af2f05769781d21eb834b7705da0b53ca00831a535cef5b4800f9098c0edc70a5ab36d875e7a7c1ffc059'
